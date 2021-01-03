a3804456df371d821f747f7c3d72ae0e1e777efe
piggy-devil <muupaza@gmail.com>
2021-01-03 17:02:04 +0700

    Implementing the Index Method for SellerCategoryController

---
+++ app/Http/Controllers/API/Seller/SellerCategoryController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\API\Seller;
+
+use App\Models\Seller;
+use Illuminate\Http\Request;
+use App\Http\Controllers\API\ApiController;
+
+class SellerCategoryController extends ApiController
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Seller $seller)
+    {
+        $categories = $seller->products()
+            ->whereHas('categories')
+            ->with('categories')
+            ->get()
+            ->pluck('categories')
+            ->collapse()
+            ->unique('id')
+            ->values();
+
+        return $this->showAll($categories);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -10,6 +10,7 @@ use App\Http\Controllers\API\Buyer\BuyerSellerController;
 use App\Http\Controllers\API\Category\CategoryController;
 use App\Http\Controllers\API\Buyer\BuyerProductController;
 use App\Http\Controllers\API\Buyer\BuyerCategoryController;
+use App\Http\Controllers\API\Seller\SellerCategoryController;
 use App\Http\Controllers\API\Buyer\BuyerTransactionController;
 use App\Http\Controllers\API\Category\CategorySellerController;
 use App\Http\Controllers\API\Transaction\TransactionController;
@@ -61,6 +62,7 @@ Route::resource('products', ProductController::class)->only(['index', 'show']);
  * Sellers
  */
 Route::resource('sellers', SellerController::class)->only(['index', 'show']);
+Route::resource('sellers.categories', SellerCategoryController::class)->only(['index', 'show']);
 Route::resource('sellers.transactions', SellerTransactionController::class)->only(['index', 'show']);
 
 /**
