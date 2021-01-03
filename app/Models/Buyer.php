dfb1c0cb7203db668f9eb9a9e28c0536ea906be2
piggy-devil <muupaza@gmail.com>
2021-01-03 14:18:55 +0700

    Resolving Buyer Using Global Scopes

--- app/Models/Buyer.php
+++ app/Models/Buyer.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Scopes\BuyerScope;
 use App\Models\Transaction;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -9,6 +10,13 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Buyer extends User
 {
     use HasFactory;
+
+    protected static function boot()
+	{
+		parent::boot();
+
+		static::addGlobalScope(new BuyerScope);
+	}
 
     public function transactions()
     {
