fd5a44d1e4fd6693ffe85b6b146d4f7d4bc9dcd1
piggy-devil <muupaza@gmail.com>
2021-01-03 08:45:21 +0700

    Creating The Factory for User, Category, Product, Transaction

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,11 +23,15 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
+            'id' => $this->faker->uuid,
             'name' => $this->faker->name,
             'email' => $this->faker->unique()->safeEmail,
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
+            'verified' => $verified = $this->faker->randomElement([User::VERIFIED_USER, User::UNVERIFIED_USER]),
+            'verification_token' => $verified == User::VERIFIED_USER ? null : User::generateVerificationCode(),
+            'admin' => $verified = $this->faker->randomElement([User::ADMIN_USER, User::REGULAR_USER]),
         ];
     }
 }
