84aed381a7b48357f9f3802d3617092b82726e76
piggy-devil <muupaza@gmail.com>
2021-01-04 03:22:26 +0700

    Solving the Problem with Factories && Fix buy uuid Transactions

--- database/factories/TransactionFactory.php
+++ database/factories/TransactionFactory.php
@@ -26,6 +26,7 @@ class TransactionFactory extends Factory
         $seller = Seller::has('products')->get()->random();
 	    $buyer = User::all()->except($seller->id)->random();
         return [
+            'id' => $this->faker->uuid,
             'quantity' => $this->faker->numberBetween(1, 3),
             'buyer_id' => $buyer->id,
             'product_id' => $seller->products->random()->id,
