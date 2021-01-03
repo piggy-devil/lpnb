ef1cbff10cde724d932ad3ebec855a09dce19ee1
piggy-devil <muupaza@gmail.com>
2021-01-03 11:10:18 +0700

    Edit local='th_TH'

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -27,10 +27,10 @@ class DatabaseSeeder extends Seeder
         Transaction::truncate();
         DB::table('category_product')->truncate();
 
-        $usersQuantity = 200;
+        $usersQuantity = 20;
         $categoriesQuantity = 30;
-        $productsQuantity = 1000;
-        $transactionsQuantity = 1000;
+        $productsQuantity = 100;
+        $transactionsQuantity = 100;
 
         User::factory()->count($usersQuantity)->create();
         Category::factory()->count($categoriesQuantity)->create();
