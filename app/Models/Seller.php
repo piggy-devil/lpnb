ef07d74dd486cf85bdbaa75de654383e442cfbca
piggy-devil <muupaza@gmail.com>
2021-01-03 16:02:14 +0700

    Resolving Seller Using Global Scopes

--- app/Models/Seller.php
+++ app/Models/Seller.php
@@ -3,12 +3,20 @@
 namespace App\Models;
 
 use App\Models\Product;
+use App\Scopes\SellerScope;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Seller extends User
 {
     use HasFactory;
+
+    protected static function boot()
+	{
+		parent::boot();
+
+		static::addGlobalScope(new SellerScope);
+	}
 
     public function products()
     {
