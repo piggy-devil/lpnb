fe32b8647901eef8f42cdff76f2d1c7eda98620b
piggy-devil <muupaza@gmail.com>
2021-01-04 04:27:01 +0700

    Filtering Responses Based on Attributes With Out Tranformdata

--- app/Http/Traits/ApiResponser.php
+++ app/Http/Traits/ApiResponser.php
@@ -19,6 +19,8 @@ trait ApiResponser
 
 	protected function showAll(Collection $collection, $code = 200)
 	{
+		$collection = $this->filterData($collection);
+		$collection = $this->sortData($collection);
 		return $this->successResponse(['data' => $collection], $code);
 	}
 
@@ -30,6 +32,29 @@ trait ApiResponser
 	protected function showMessage($message, $code = 200)
 	{
 		return $this->successResponse(['data' => $message], $code);
+	}
+
+	protected function filterData(Collection $collection)
+	{
+		foreach (request()->query() as $query => $value) {
+
+			if (isset($attribute, $value)) {
+				$collection = $collection->where($attribute, $value);
+			}
+		}
+
+		return $collection;
+	}
+
+	protected function sortData(Collection $collection)
+	{
+		if (request()->has('sort_by')) {
+			$attribute = request()->sort_by;
+
+			$collection = $collection->sortBy->{$attribute};
+		}
+
+		return $collection;
 	}
 
 } 
