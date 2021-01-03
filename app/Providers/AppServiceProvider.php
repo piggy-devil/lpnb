38c4207abf5a619ae1f418173153352ceb8dcfb4
piggy-devil <muupaza@gmail.com>
2021-01-04 03:29:34 +0700

    Send Verification for Email Changed Using Events

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use App\Models\User;
 use App\Models\Product;
 use App\Mail\UserCreated;
+use App\Mail\UserMailChanged;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\ServiceProvider;
 
@@ -29,6 +30,12 @@ class AppServiceProvider extends ServiceProvider
     {
         User::created(function($user) {
             Mail::to($user)->send(new UserCreated($user));
+        });
+
+        User::updated(function($user) {
+            if ($user->isDirty('email')) {
+                Mail::to($user)->send(new UserMailChanged($user));
+            }
         });
         
         Product::updated(function($product) {
