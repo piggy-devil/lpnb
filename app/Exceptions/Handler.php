8d2e7ab67d785fec403967a5e39fadaa230dee50
piggy-devil <muupaza@gmail.com>
2021-01-03 14:04:37 +0700

    Handling AuthenticationException

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,6 +4,7 @@ namespace App\Exceptions;
 
 use Throwable;
 use App\Http\Traits\ApiResponser;
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Validation\ValidationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
@@ -37,6 +38,10 @@ class Handler extends ExceptionHandler
             $modelName = strtolower(class_basename($exception->getModel()));
 
             return $this->errorResponse("Does not exists any {$modelName} with the specified identificator", 404);
+        }
+
+        if ($exception instanceof AuthenticationException) {
+            return $this->unauthenticated($request, $exception);
         }
         
         return parent::render($request, $exception);
@@ -76,5 +81,17 @@ class Handler extends ExceptionHandler
         $errors = $e->validator->errors()->getMessages();
 
         return $this->errorResponse($errors, 422);
+    }
+
+    /**
+     * Convert an authentication exception into an unauthenticated response.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Auth\AuthenticationException  $exception
+     * @return \Illuminate\Http\Response
+     */
+    protected function unauthenticated($request, AuthenticationException $exception)
+    {
+        return $this->errorResponse('Unauthenticated.', 401);
     }
 }
