0bc78593ede9531a3170b90295df468b9648b44f
piggy-devil <muupaza@gmail.com>
2021-01-03 17:13:38 +0700

    Implementing the Store, Update, Destroy Method for SellerProductController

--- app/Http/Controllers/API/Seller/SellerProductController.php
+++ app/Http/Controllers/API/Seller/SellerProductController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers\API\Seller;
 
-use App\Http\Controllers\Controller;
+use App\Models\User;
 use App\Models\Seller;
+use App\Models\Product;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class SellerProductController extends Controller
 {
@@ -26,9 +29,26 @@ class SellerProductController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(Request $request, User $seller)
     {
-        //
+        $rules = [
+            'name' => 'required',
+            'description' => 'required',
+            'quantity' => 'required|integer|min:1',
+            'image' => 'required|image',
+        ];
+
+        $this->validate($request, $rules);
+
+        $data = $request->all();
+
+        $data['status'] = Product::UNAVAILABLE_PRODUCT;
+        $data['image'] = '1.jpg';
+        $data['seller_id'] = $seller->id;
+
+        $product = Product::create($data);
+
+        return $this->showOne($product);
     }
 
     /**
@@ -49,9 +69,39 @@ class SellerProductController extends Controller
      * @param  \App\Models\Seller  $seller
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Seller $seller)
+    public function update(Request $request, Seller $seller, Product $product)
     {
-        //
+        $rules = [
+            'quantity' => 'integer|min:1',
+            'status' => 'in:' . Product::AVAILABLE_PRODUCT . ',' . Product::UNAVAILABLE_PRODUCT,
+            'image' => 'image',
+        ];
+
+        $this->validate($request, $rules);
+
+        $this->checkSeller($seller, $product);
+
+        $product->fill($request->intersect([
+            'name',
+            'description',
+            'quantity',
+        ]));
+
+        if ($request->has('status')) {
+            $product->status = $request->status;
+
+            if ($product->isAvailable() && $product->categories()->count() == 0) {
+                return $this->errorResponse('An active product must have at least one category', 409);
+            }
+        }
+
+        if ($product->isClean()) {
+            return $this->errorResponse('You need to specify a different value to update', 422);
+        }
+
+        $product->save();
+
+        return $this->showOne($product);
     }
 
     /**
@@ -60,8 +110,19 @@ class SellerProductController extends Controller
      * @param  \App\Models\Seller  $seller
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Seller $seller)
+    public function destroy(Seller $seller, Product $product)
     {
-        //
+        $this->checkSeller($seller, $product);
+
+        $product->delete();
+
+        return $this->showOne($product);
+    }
+
+    protected function checkSeller(Seller $seller, Product $product)
+    {
+        if ($seller->id != $product->seller_id) {
+            throw new HttpException(422, 'The specified seller is not the actual seller of the product');            
+        }
     }
 }
