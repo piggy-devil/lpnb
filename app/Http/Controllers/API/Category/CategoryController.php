ae1d7bd951e2925f4d5a491ed7455b5570d50b60
piggy-devil <muupaza@gmail.com>
2021-01-04 10:42:46 +0700

    Using client_credentials Grant to Protect Some Routes (Category.index)

--- app/Http/Controllers/API/Category/CategoryController.php
+++ app/Http/Controllers/API/Category/CategoryController.php
@@ -8,6 +8,11 @@ use App\Http\Controllers\API\ApiController;
 
 class CategoryController extends ApiController
 {
+    public function __construct()
+    {
+        $this->middleware('client.credentials')->only(['index', 'show']);
+    }
+
     /**
      * Display a listing of the resource.
      *
